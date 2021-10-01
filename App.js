21e87d5441ae495ab583e54ff9529ef81551f21c
FlorBergesio <ing.florencia.bergesio@gmail.com>
2021-10-01 11:41:21 -0300

    Installed Bottom Tabs

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import CoinsStack from './src/components/coins/CoinsStack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
+import Colors from './src/res/colors';
+
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator
+        screenOptions={{
+          tabBarStyle:{
+            backgroundColor: Colors.blackPearl
+          },
+          headerShown: false
+        }}
+      >
+        <Tabs.Screen 
+          name="Coins"
+          component={ CoinsStack }
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Image 
+                source={ require('./src/assets/bank.png') }
+                style={{ tintColor: color, width: size, height: size }}
+              />
+            )
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
