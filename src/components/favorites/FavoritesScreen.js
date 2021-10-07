6912d5a683af7dd8b1e7488fb3311311c5b80eb1
FlorBergesio <ing.florencia.bergesio@gmail.com>
2021-10-07 15:50:48 -0300

    Added list of favorite coins

--- src/components/favorites/FavoritesScreen.js
+++ src/components/favorites/FavoritesScreen.js
@@ -1,12 +1,51 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useCallback } from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+import { useFocusEffect } from '@react-navigation/native';
 import Colors from '../../res/colors';
 import FavoritesEmptyState from "./FavoritesEmptyState";
+import CoinsItem from "../coins/CoinsItem";
+import Storage from '../../libs/storage';
 
-const FavoritesScreen = () => {
+const FavoritesScreen = ( props ) => {
+    const [ favorites, setFavorites ] = useState( [] );
+
+    useFocusEffect( useCallback( () => {
+        const cleanup = getFavorites();
+        return () => cleanup;
+    }, [] ) );
+
+    const getFavorites = async () => {
+        try {
+            const allKeys = await Storage.instance.getAllKeys();
+            const favKeys = allKeys.filter( key => key.includes("favorite-") );
+            const favoritesFull = await Storage.instance.multiGet( favKeys );
+            
+            const favoritesMapped = favoritesFull.map( fav => JSON.parse(fav[1]) );
+            setFavorites( [...favoritesMapped] );
+        } catch ( err ) {
+            console.log( "Get favorites err", err );
+        }
+    };
+
+    const handlePress = ( coin ) => {
+        props.navigation.navigate("Coin Detail", { coin });
+    };
+
     return (
         <View style={ styles.container }>
-            <FavoritesEmptyState />
+            {
+                favorites.length > 0
+                ? <FlatList
+                        data={ favorites }
+                        renderItem={ ({ item }) => (
+                            <CoinsItem
+                                item={ item }
+                                onPress={ () => handlePress( item ) }
+                            />
+                        )}
+                    />
+                : <FavoritesEmptyState />
+            }            
         </View>
     );
 };
